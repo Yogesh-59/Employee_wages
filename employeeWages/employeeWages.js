df1c2187425de03b83a5c8c26f55232d50c1d7cb
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-03-04 10:03:28 +0530

    [Deepraj] add. java Script code that find the employee is present or not

---
+++ employeeWages/employeeWages.js
@@ -0,0 +1,9 @@
+const IS_ABSENT=1
+let empCheck =Math.floor(Math.random()*2);
+if(empCheck==IS_ABSENT){
+    console.log("Employee is Absent");
+    return ;
+}
+else{
+    console.log("Employee is Present");
+}
