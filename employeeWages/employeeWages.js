79a620da822dc2a2eee47f06d9aa6830a5b609d6
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-03-04 10:11:08 +0530

    [Deepraj] add. java Script code that calculate the employee wages

--- employeeWages/employeeWages.js
+++ employeeWages/employeeWages.js
@@ -1,9 +1,28 @@
 const IS_ABSENT=1
-let empCheck =Math.floor(Math.random()*2);
-if(empCheck==IS_ABSENT){
+let emp =Math.floor(Math.random()*2);
+if(emp==IS_ABSENT){
     console.log("Employee is Absent");
     return ;
 }
 else{
     console.log("Employee is Present");
 }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2; 
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;          
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 2)+1; 
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
